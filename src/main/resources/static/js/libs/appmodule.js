8853db91f6565968817eaaa42e234a7af4e58ad2
GiovannySissa <helber.sissa@mail.escuelaing.edu.co>
2015-11-12 11:49:53 -0500

    create canvas

--- src/main/resources/static/js/libs/appmodule.js
+++ src/main/resources/static/js/libs/appmodule.js
@@ -3,13 +3,15 @@
     app.controller('plan_control', 
     function($scope,$http){
 		 $scope.listnames=[];
-		 $scope.blueprint="";
+		 $scope.nameblueprint="";
+		 $scope.blueprint = { points : "points", name : "name"
+		 };
 		 
 		 $scope.loadData = function() {
-			 alert("entro");
+			 
 			var configList = {
 				method: "GET",
-				url: "http://localhost:8080/blueprints"
+				url: "http://10.2.67.82:8080/blueprints"
 			};
 
 			var response=$http(configList);
@@ -23,9 +25,32 @@
 			});
 		};
 		 
+		$scope.drawBluePrint = function(){
+			var cnv= document.getElementById("canvas");
+			var ctx = cnv.getContext("2d");
+			$scope.selectedBlueprint();
+		}; 
 		 
+		$scope.selectedBlueprint = function(){
+			alert($scope.nameblueprint);
+			var configList = {
+				method: "GET",
+				url: "http://10.2.67.82:8080/blueprints/" + $scope.nameblueprint
+			};
+			var response=$http(configList);
+
+			response.success(function(data, status, headers, config) {
+				alert("respose");
+				$scope.blueprint = data; 
+				alert($scope.blueprint.name);
+			});
+
+			response.error(function(data, status, headers, config) {
+				alert("The petition has failed. HTTP Status:"+status);
+			});
+		}; 
 		 
-		});
+	});
 	
 })();
 
