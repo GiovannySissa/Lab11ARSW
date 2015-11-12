88277b1ecb0f70b95e84b70717deb4e0c4c8ba6c
duvanbarrera <duvan.barrera@mail.escuelaing.edu.co>
2015-11-12 11:04:06 -0500

    cargar planos

--- src/main/resources/static/js/libs/appmodule.js
+++ src/main/resources/static/js/libs/appmodule.js
@@ -1,5 +1,32 @@
 (function () {
     var app = angular.module('modone', []);
+    app.controller('plan_control', 
+    function($scope,$http){
+		 $scope.listnames=[];
+		 $scope.blueprint="";
+		 
+		 $scope.loadData = function() {
+			 alert("entro");
+			var configList = {
+				method: "GET",
+				url: "http://localhost:8080/blueprints"
+			};
+
+			var response=$http(configList);
+
+			response.success(function(data, status, headers, config) {
+				$scope.listnames = data;
+			});
+
+			response.error(function(data, status, headers, config) {
+				alert("The petition has failed. HTTP Status:"+status);
+			});
+		};
+		 
+		 
+		 
+		});
+	
 })();
 
 
